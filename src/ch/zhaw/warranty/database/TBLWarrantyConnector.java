e8dc60dcb03c0f7b9e25ecd53434b5320bb565e6
Andreas Gruenenfelder <gruenean@students.zhaw.ch>
2012-10-13 14:54:47 +0200

    interface for table warranty created. support for insert, deleteAll, getAll added

--- src/ch/zhaw/warranty/database/TBLWarrantyConnector.java
+++ src/ch/zhaw/warranty/database/TBLWarrantyConnector.java
@@ -1,12 +1,18 @@
 package ch.zhaw.warranty.database;
 
+import java.util.ArrayList;
+
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import ch.zhaw.warranty.card.WarrantyCard;
 
+/**
+ * This class manages all calls to the database table 'warranty'
+ */
 public class TBLWarrantyConnector {
 	private SQLiteDatabase db;
 	private SQLiteOpenHelper dbHelper;
@@ -15,38 +21,85 @@ public class TBLWarrantyConnector {
 		dbHelper = new TBLWarrantyHelper(context,TBLWarrantyHelper.TBL_NAME,null,1);
 	}
 	
-	private void open() throws SQLException {
+	/**
+	 * Opens an r/w connection to the sqlite db
+	 * @throws SQLException
+	 */
+	private void openDB() throws SQLException {
 		db = dbHelper.getWritableDatabase();
 	}
 	
-	private void close() {
+	/**
+	 * closes the connection to the sqlite db
+	 */
+	private void closeDB() {
 		if (db != null) {
 			db.close();
 		}
 	}
 	
+	/**
+	 * This method will be called each time a new warranty card is created
+	 * @param card	newly created warranty card
+	 */
 	public void insertWarrantyCard(WarrantyCard card){
 		ContentValues values = new ContentValues();
-		values.put("title", card.getTitle());
+		values.put(TBLWarrantyHelper.CLMN_TITLE, card.getTitle());
+		values.put(TBLWarrantyHelper.CLMN_DESC, card.getDescription());
+		values.put(TBLWarrantyHelper.CLMN_CREATEDAT, card.getCreatedAt());
+		values.put(TBLWarrantyHelper.CLMN_VLDTIL, card.getValidUntil());
+		values.put(TBLWarrantyHelper.CLMN_PRICE, card.getPrice());
+		values.put(TBLWarrantyHelper.CLMN_RESSELLER,card.getReseller());
 		
-		open();
+		openDB();
 			db.insert(TBLWarrantyHelper.TBL_NAME, null, values);
-		close();
+		closeDB();
 	}
 
+	/**
+	 * This method is used to update a particular warranty card
+	 * @param card	card to update
+	 */
 	public void updateWarrantyCard(WarrantyCard card) {
 		ContentValues values = new ContentValues();
 		values.put("title", card.getTitle());
 		values.put("description", card.getDescription());
 		
-		
-		open();
+		openDB();
 			db.update(TBLWarrantyHelper.TBL_NAME, values, "_id="+card.get_id(), null);
-		close();
+		closeDB();
 	}
 	
-	public void listAllCards() {
-		db.query(TBLWarrantyHelper.TBL_NAME, new String[] {TBLWarrantyHelper.CLMN_TITLE, TBLWarrantyHelper.CLMN_VLDTIL}, null, null, null, null,TBLWarrantyHelper.CLMN_TITLE);
+	/**
+	 * This method will return all saved warranty cards
+	 * @return	all saved warranty cards
+	 */
+	public ArrayList<String> getAllCards() {
+		System.out.println("list all cards");
+		openDB();
+
+//		ArrayList<WarrantyCard> cards = new ArrayList<WarrantyCard>();
+		ArrayList<String> cards = new ArrayList<String>();
+//		Cursor cursor = db.query(TBLWarrantyHelper.TBL_NAME, new String[] {TBLWarrantyHelper.CLMN_TITLE, TBLWarrantyHelper.CLMN_VLDTIL}, null, null, null, null,TBLWarrantyHelper.CLMN_TITLE);
+		Cursor cursor = db.query(TBLWarrantyHelper.TBL_NAME, new String[] {TBLWarrantyHelper.CLMN_TITLE} , null, null, null, null,TBLWarrantyHelper.CLMN_TITLE);
+		cursor.moveToFirst();
+
+		while(!cursor.isAfterLast()) {
+			cards.add(cursor.getString(0));
+			cursor.moveToNext();
+		}
+		cursor.close();
+		closeDB();
+		return cards;
 	}
 	
+	/**
+	 * Deletes all saved cards
+	 */
+	public void deleteAllCards() {
+		openDB();
+		db.delete(TBLWarrantyHelper.TBL_NAME, null, null);
+		closeDB();
+		
+	}
 }
--- src/ch/zhaw/warranty/database/TBLWarrantyHelper.java
+++ src/ch/zhaw/warranty/database/TBLWarrantyHelper.java
@@ -15,13 +15,21 @@ public class TBLWarrantyHelper extends SQLiteOpenHelper {
 	public static final String CLMN_PRICE = "price";
 	public static final String CLMN_RESSELLER = "reseller";
 	
+	/**
+	 * Creates the table 'warranty' 
+	 * 
+	 * @param context
+	 * @param name
+	 * @param factory
+	 * @param version
+	 */
 	public TBLWarrantyHelper(Context context, String name, CursorFactory factory, int version) {
 		super(context,name,factory,version);
 	}
 	
 	// DB creation statement
 	private static final String DB_CREATE="create table " + TBL_NAME + " (" +
-			CLMN_ID + " int primary key autoincrement," + 
+			CLMN_ID + " integer primary key autoincrement," + 
 			CLMN_TITLE + " text," + 
 			CLMN_DESC + " text," + 
 			CLMN_IMGPATH + " text," + 
@@ -30,11 +38,17 @@ public class TBLWarrantyHelper extends SQLiteOpenHelper {
 			CLMN_PRICE + " real," +
 			CLMN_RESSELLER + " text);";
 	
+	/* (non-Javadoc)
+	 * @see android.database.sqlite.SQLiteOpenHelper#onCreate(android.database.sqlite.SQLiteDatabase)
+	 */
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		db.execSQL(DB_CREATE);
 	}
 
+	/* (non-Javadoc)
+	 * @see android.database.sqlite.SQLiteOpenHelper#onUpgrade(android.database.sqlite.SQLiteDatabase, int, int)
+	 */
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		db.execSQL("DROP TABLE IF EXISTS " + TBL_NAME);
